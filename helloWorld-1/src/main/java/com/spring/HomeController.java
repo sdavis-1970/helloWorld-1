6bb66c6295d68dad45f1e00b85f7e6362b7142bd
shery <shery@DESKTOP-46D4GS0>
2020-07-08 09:48:57 -0500

    commit-1

---
+++ helloWorld-1/src/main/java/com/spring/HomeController.java
@@ -0,0 +1,19 @@
+package com.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HomeController 
+{
+	@RequestMapping("/home")
+	public String showHomePage()
+	{
+		System.out.println("Controller invoked");
+
+		return "home";
+		
+	}
+
+}
